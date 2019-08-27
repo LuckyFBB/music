975dda1905cb0ce1968b147b1012ba3faf9fb8e6
LuckyFBB <976060700@qq.com>
2019-08-27 21:26:06 +0800

    Updates

---
+++ precache-manifest.5c57c14dbede9447ea063642d17d54bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde51ed980a55dd50e98207ea52e7c4a",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "4fdf7745bffb7a488b29",
+    "url": "/music/static/css/2.df244b35.chunk.css"
+  },
+  {
+    "revision": "633e936e47c1ab6095b9",
+    "url": "/music/static/css/main.8d33eae3.chunk.css"
+  },
+  {
+    "revision": "4fdf7745bffb7a488b29",
+    "url": "/music/static/js/2.25e9f22e.chunk.js"
+  },
+  {
+    "revision": "633e936e47c1ab6095b9",
+    "url": "/music/static/js/main.b69ca1e1.chunk.js"
+  },
+  {
+    "revision": "3321ad820ee41c1cac90",
+    "url": "/music/static/js/runtime~main.4a84191b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.69eab0bd4ec96a75a9f8dbe360f3cece.js"
+  "/music/precache-manifest.5c57c14dbede9447ea063642d17d54bf.js"
 );
 
 self.addEventListener('message', (event) => {
